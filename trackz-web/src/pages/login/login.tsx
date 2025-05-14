2ebfa50c7d17241fd2253ca6fe0b3a21bbd92da1
Antonio Anderson <antonio.silva@ufpi.edu.br>
2025-05-14 20:33:49 -0300

    Implementando tela de login com validação de campos

---
+++ trackz-web/src/pages/login/login.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import Botao from '../../components/Botao/botao';
+import styles from './login.module.css'; // Caminho relativo, correto
+import FormularioLogin from '../../components/Formulario/FormularioLogin';
+
+
+export function Login() {
+  return (
+    <div className={styles.container}>
+        <h1 className={styles.header}>Login</h1>
+        <FormularioLogin />
+    </div>
+  );
+}
+export default Login;
