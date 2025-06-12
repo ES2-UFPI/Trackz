c31d918acacf7f2715e35815be2ee54c14183f53
Antonio Anderson <antonio.silva@ufpi.edu.br>
2025-06-12 06:28:14 -0300

    fix(ci): Corrige erros de linting do backend

--- trackz-api/eslint.config.mjs
+++ trackz-api/eslint.config.mjs
@@ -1,35 +1,24 @@
+// trackz-api/eslint.config.mjs
 // @ts-check
 import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
   eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
+  ...tseslint.configs.recommendedTypeChecked, // Use a configuração recomendada com tipos
   {
     languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      ecmaVersion: 5,
-      sourceType: 'module',
       parserOptions: {
-        projectService: true,
+        project: true, // Diz ao linter para usar seu tsconfig.json
         tsconfigRootDir: import.meta.dirname,
       },
     },
   },
   {
+    // Ignora a regra de variáveis não utilizadas para arquivos de configuração
+    files: ['eslint.config.mjs'],
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unused-vars': 'off',
     },
   },
 );
